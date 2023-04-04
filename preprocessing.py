b6391054811a3418807d74a3a880dc9cfd19650a
Naibed-cloud <davidnicolae5858@gmail.com>
2023-04-04 03:27:51 +0300

    Solved coodrinate rescaling bug

--- preprocessing.py
+++ preprocessing.py
@@ -36,7 +36,7 @@ def extract_positive_patches():
     
     for _, row in df.iterrows():
         if row['stain'] != 'HE':
-            break
+            continue
         slide_path = os.path.join(DATA_DIR, row['stain'], row['tma_id'])
         slide = openslide.open_slide(slide_path)
         level = slide.get_best_level_for_downsample(DOWNSAMPLE_FACTOR)
@@ -44,11 +44,12 @@ def extract_positive_patches():
         roi_size = (row['xe'] - row['xs'], row['ye'] - row['ys'])
         roi = slide.read_region(roi_start, level, roi_size)
         
+        # print(roi.size)
         # whole_slide = slide.read_region((0, 0), level, slide.level_dimensions[level])
         # roi = roi.resize((int(roi_size[0] / downsample_factor), int(roi_size[1] / downsample_factor)))
-        
-        for y in range(0, roi.size[1], patch_size[1]):
-            for x in range(0, roi.size[0], patch_size[0]):
+
+        for y in range(0, roi.size[1] - patch_size[1], patch_size[1]):
+            for x in range(0, roi.size[0] - patch_size[0], patch_size[0]):
                 patch_rgb = roi.crop((x, y, x + patch_size[0], y + patch_size[1])).convert('RGB')
                 patch_hsv = cv2.cvtColor(np.array(patch_rgb), cv2.COLOR_RGB2HSV)
                 patch_hsv = cv2.GaussianBlur(patch_hsv, (5, 5), 0)
@@ -59,16 +60,16 @@ def extract_positive_patches():
                 patch_brightness = np.mean(brightness_channel / 255)
                 
                 if patch_saturation > SATURATION_THRESHOLD and patch_brightness > BRIGHTNESS_THRESHOLD:
-                    # print(index, patch_saturation, patch_brightness)
                     patch_name = 'img_' + str(index) + '.jpeg'
                     patch_rgb.save(os.path.join(POSITIVE_PATCHES_DIR, patch_name), 'jpeg')
                     labels_dict[index] = (patch_name, 1)
                     index += 1
                 else:
-                    patch_name = 'img_' + str(discard_index) + '.jpeg'
-                    patch_rgb.save(os.path.join(DISCARDED_DIR, patch_name), 'jpeg')
-                    labels_dict[discard_index] = (patch_name, 1)
+                    # patch_name = 'img_' + str(discard_index) + '.jpeg'
+                    # patch_rgb.save(os.path.join(DISCARDED_DIR, patch_name), 'jpeg')
+                    # labels_dict[discard_index] = (patch_name, 1)
                     discard_index += 1
+    print(discard_index)
                     
     # with open(os.path.join(DATA_DIR, 'labels.json'), 'w') as f:
     #     json.dump(labels_dict, f, indent=4)
@@ -81,19 +82,18 @@ def parse_annotations(csv_path):
     annotations = {}
     df = pd.read_csv(csv_path)
     for _, row in df.iterrows():
-        if row['stain'] != 'HE':
-            break
-        roi = (row['xs'], row['ys'], row['xe'], row['ye'])
-        if row['tma_id'] in annotations:
-            annotations[row['tma_id']].append(roi)
-        else:
-            annotations[row['tma_id']] = [roi]
+        if row['stain'] == 'HE':
+            roi = (row['xs'], row['ys'], row['xe'], row['ye'])
+            if row['tma_id'] in annotations:
+                annotations[row['tma_id']].append(roi)
+            else:
+                annotations[row['tma_id']] = [roi]
     return annotations
 
 def extract_negative_patches():
     csv_path = os.path.join(DATA_DIR, ANNOTATIONS_PATH)
     annotations = parse_annotations(csv_path)
-    df = pd.read_csv(csv_path)
+
     if not os.path.exists(NEGATIVE_PATCHES_DIR):
         os.makedirs(NEGATIVE_PATCHES_DIR)
     if not os.path.exists(DISCARDED_DIR):
@@ -103,21 +103,23 @@ def extract_negative_patches():
     labels_dict = {}
     index = 0
     discard_index = 0
-
+    
     for tma_id, regions in annotations.items():
         slide_path = os.path.join(DATA_DIR, 'HE', tma_id)
         slide = openslide.open_slide(slide_path)
         level = slide.get_best_level_for_downsample(DOWNSAMPLE_FACTOR)
-        print(regions)
+        scale_factor = 1 / int(slide.level_downsamples[level])
         roi = slide.read_region((0, 0), level, slide.level_dimensions[level])
         
-        for y in range(0, roi.size[1], patch_size[1]):
-            for x in range(0, roi.size[0], patch_size[0]):
+        for y in range(0, roi.size[1] - patch_size[1], patch_size[1]):
+            for x in range(0, roi.size[0] - patch_size[0], patch_size[0]):
                 patch = box(x, y, x + patch_size[0], y + patch_size[1])
                 intersection = False
                 for region in regions:
-                    if patch.intersects(box(*region)):
+                    if patch.intersects(box(region[0] * scale_factor, region[1] * scale_factor,
+                                            region[2] * scale_factor, region[3] * scale_factor)):
                         intersection = True
+                        discard_index += 1
                         break
                 if intersection == True:
                     continue
@@ -135,11 +137,12 @@ def extract_negative_patches():
                     patch_rgb.save(os.path.join(NEGATIVE_PATCHES_DIR, patch_name), 'jpeg')
                     labels_dict[index] = (patch_name, 1)
                     index += 1
-                # else:
+                else:
                 #     patch_name = 'img_' + str(discard_index) + '.jpeg'
                 #     patch_rgb.save(os.path.join(DISCARDED_DIR, patch_name), 'jpeg')
                 #     labels_dict[discard_index] = (patch_name, 1)
-                #     discard_index += 1
+                    discard_index += 1
+    print(discard_index)
 
 if __name__ == '__main__':
     extract_positive_patches()
